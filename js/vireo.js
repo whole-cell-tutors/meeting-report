26b24b5b7931d84052d5fc1d59320602a3300547
Michael Hucka <mhucka@caltech.edu>
2015-05-23 14:48:56 -0700

    Added more <meta> cache tags.

--- js/vireo.js
+++ js/vireo.js
@@ -149,7 +149,11 @@ function openLog(base, path) {
     var win = window.open('');
     $.get(url, function(data) {
         $(win.document.body).html('<html><head>' 
-                                  + '<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />'
+                                  + '<meta http-equiv="Cache-Control" content="max-age=0" />'
+                                  + '<meta http-equiv="Cache-Control" content="no-cache" />'
+                                  + '<meta http-equiv="Cache-Control" content="no-store" />'
+                                  + '<meta http-equiv="Cache-Control" content="must-revalidate" />'
+                                  + '<meta http-equiv="Vary" content="*" />'
                                   + '<meta http-equiv="Pragma" content="no-cache" />'
                                   + '<meta http-equiv="Expires" content="0" />'
                                   + '</head>'
